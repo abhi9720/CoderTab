c164c41d3bb37bd40c2d1a1ebb5bc383f94f9714
abhi9720 <abhishektiwari9720@gmail.com>
2023-02-27 15:28:49 +0530

    json updated

--- src/FormatterComponent/JsonEditor.jsx
+++ src/FormatterComponent/JsonEditor.jsx
@@ -13,6 +13,10 @@ import copy from "copy-to-clipboard";
 import { FileUploader } from "react-drag-drop-files";
 import { Link } from "react-router-dom";
 
+import jsonpath from 'jsonpath';
+import { FaDollarSign } from "react-icons/fa";
+
+
 const options = {
   minimap: {
     enabled: false
@@ -28,14 +32,76 @@ function App() {
   // const [processing, setProcessing] = useState(false);
   const [syntaxError, setSyntaxError] = useState(null);
   const [error, setError] = useState(false)
-  const [lang, setLang] = useState('json')
+  const [lang, setLang] = useState('')
+
+
+
+
+  /* Json query Hanlder :***************************************/
+
+  const [query, setQuery] = useState('');
+  const [queryoutput, setQueryOutput] = useState('')
+
+
+  function handleKeyDown(e) {
+    e.preventDefault();
+    getValueFrompath();
+
+  }
+
+  function isValidQueryPath(obj, path) {
+    const keys = path.split('.');
+    let currentObj = obj;
+
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      if (!currentObj.hasOwnProperty(key)) {
+        return false;
+      }
+      currentObj = currentObj[key];
+    }
+
+    return true;
+  }
+
+  function getValueFrompath() {
+    if (inputJsonData && query) {
+      const data = JSON.parse(inputJsonData);
+      if (typeof data === "object") {
+        try {
+          if (!isValidQueryPath(data, query)) {
+            console.log("invalid json")
+            throw "Invalid Json Query";
+          }
+          const result = jsonpath.query(data, query);
+
+          setQueryOutput(JSON.stringify(result[0], null, 2) || "Invalid Json Query Path");
+
+          setError(false)
+          setSyntaxError('')
+          setLang('')
+        }
+        catch (err) {
+          setError(true)
+          setSyntaxError(new String(err))
+        }
+      }
+    }
+
+
+  }
+
+
+
+
+  /************************************************************ */
 
   const handleInputChange = (action, data) => {
 
     switch (action) {
       case "code": {
         try {
-          setInputJsonData(data);
+          setInputJsonData(JSON.parse(JSON.stringify(data)));
         }
         catch (err) {
           console.log(err)
@@ -43,7 +109,6 @@ function App() {
         break;
       }
       default: {
-        setError(true)
         console.warn("case not handled!", action, data);
       }
     }
@@ -53,11 +118,13 @@ function App() {
     try {
       const formattedJson = JSON.stringify(JSON.parse(inputJsonData), null, 2);
       setOutputJsonData(formattedJson);
-
       setLang("json")
-
-    } catch (error) {
-      setOutputJsonData("Invalid JSON Syntax");
+      setError(false)
+      setSyntaxError('');
+    } catch (err) {
+      console.log(err.SyntaxError)
+      setError(true);
+      setSyntaxError("Invalid JSON Syntax")
       showErrorToast("Invalid JSON Syntax");
     }
   };
@@ -69,10 +136,9 @@ function App() {
       setSyntaxError(null);
       setError(false)
       showSuccessToast("Json Valid", 2000)
-
       setLang("json")
     } catch (e) {
-      console.log("error", e.message)
+
       setSyntaxError(e.message);
       setError(true);
       showSuccessToast("Json Invalid", 2000)
@@ -97,17 +163,31 @@ function App() {
   };
 
   function parseCSV(data) {
-    const results = [];
-    const headers = Object.keys(data);
-    console.log(headers)
-    results.push(headers.join(','));
-    const values = headers.map((header) => {
-      return data[header];
-    });
+    // const results = [];
+    // const headers = Object.keys(data);
+    // console.log(headers)
+    // results.push(headers.join(','));
+    // const values = headers.map((header) => {
+    //   return data[header];
+    // });
 
     setLang("csv");
-    results.push(values.join(','));
-    return results.join('\n');
+    // results.push(values.join(','));
+    // return results.join('\n');
+    const rows = [];
+    let parseddata = JSON.parse(data)
+    console.log(typeof parseddata)
+    const headers = Object.keys(parseddata);
+    rows.push(headers);
+    const row = [];
+    headers.forEach(key => {
+      row.push(parseddata[key]);
+    });
+    rows.push(row);
+    const csv = rows.map(row => row.join(',')).join('\n');
+
+    return csv;
+
   }
 
   function convertToCSV() {
@@ -143,19 +223,22 @@ function App() {
   return (
     <div className="App">
 
-      <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-3">
-        <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-3">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
           <Link to="/" className="redirecthome">
             <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mr-5">
               <FaHome fontSize={18} color="black" />
             </button>
           </Link>
-          <img src="https://user-images.githubusercontent.com/68281476/221413170-43e808c4-48a4-457f-b61d-0b0dcd434ced.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+          <img src="https://user-images.githubusercontent.com/68281476/221413170-43e808c4-48a4-457f-b61d-0b0dcd434ced.png" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
 
 
-          <span class="font-semibold text-xl tracking-tight">JSON Validator
+          <span className="font-semibold text-xl tracking-tight">JSON Validator
           </span>
         </div>
+
+
+
 
 
         <div className="fileuploader">
@@ -169,10 +252,24 @@ function App() {
           </div>
         </button>
 
+
+        <form onSubmit={handleKeyDown}>
+          <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none font-bold">
+              <FaDollarSign></FaDollarSign>.
+            </div>
+            <input autoComplete="off" value={query} onChange={(e) => setQuery(e.target.value)} type="search" id="search" class="block p-4 pl-10 text-sm text-gray-900 border border-teal-300 rounded-lg bg-teal-50 focus:ring-teal-500 focus:border-teal-500 dark:bg-teal-700 dark:border-teal-600 dark:placeholder-teal-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500 focus:outline-none docus jsonqueryinput" placeholder="Json Query Path" required />
+            <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800 ">Search</button>
+          </div>
+        </form>
+
+
+
+
+
       </nav>
       <div className="pannelWrapper">
-
-
         <div className="inputpanel">
           <CodeEditorWindow
             className="inputbox"
@@ -190,9 +287,9 @@ function App() {
         <div className="Actionbtn">
 
           {
-            outputJsonData &&
-            <div class="bg-blue-100 text-blue-800  font-bold text-lg mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 flex justify-center items-center gap-2">
-              <span>Json</span>  <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  <span className="capitalize">{lang}</span>
+            outputJsonData && lang &&
+            <div className="bg-blue-100 text-blue-800  font-bold text-lg mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 flex justify-center items-center gap-2">
+              <span>Json</span>  <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>  <span className="capitalize">{lang}</span>
             </div>
 
 
@@ -216,17 +313,27 @@ function App() {
 
         <div className="outputpanel">
 
-          {syntaxError ? (
+          {error ? (
             <div className="DisplayActionmsg">
-              <h2>Validation Errors:</h2>
+              <h2>Errors:</h2>
 
-              {syntaxError}
+              <pre>
+                {syntaxError}
+              </pre>
             </div>
           ) :
             <>
-              <CodeEditorWindow options={options} language={lang} className="outputbox" code={outputJsonData} ></CodeEditorWindow>
-
-              {/* <textarea className="outputbox" value={output} readOnly /> */}
+              {
+                !lang ?
+                  <div className="DisplayActionmsg">
+                    <pre>
+                      {queryoutput}
+                    </pre>
+                  </div>
+                  : <>
+                    <CodeEditorWindow options={options} language={lang} className="outputbox" code={outputJsonData} ></CodeEditorWindow>
+                  </>
+              }
             </>
           }
 
